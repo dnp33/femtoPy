9540db0a2c30e0d4106376faf6a65125c62d4e2d
David <purschke@ualberta.ca>
2017-01-16 18:20:41 -0700

    third commit

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
-setup(name='femtoPy'),
+setup(name='femtoPy',
       version='0.1',
       description='Python code for data analysis in the ultrafast nanotools lab',
-    
+      url='http://github.com/dnp33/femtoPy',
+      author='David N. Purschke',
+      packages=['femtoPy'],
+      zip_safe=False)
