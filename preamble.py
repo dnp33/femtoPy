1dbdb63f18c25e2df339d575e2b1419f8498609d
David <purschke@ualberta.ca>
2017-01-16 18:09:47 -0700

    First commit

---
+++ preamble.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize']=10,8
+
+plt.rcParams['axes.labelsize']=20
+plt.rcParams['axes.titlesize']=25
+plt.rcParams['axes.grid']=True
+plt.rcParams['axes.linewidth']=4
+
+plt.rcParams['lines.linewidth']=3
+
+plt.rcParams['xtick.labelsize']=15
+plt.rcParams['ytick.labelsize']=15
+
+plt.rcParams['legend.fontsize']=20
+plt.rcParams['legend.fancybox']=True
+
+
+#from imp import find_module
+#from imp import load_module
+
+#from os.path import expanduser
+#home=expanduser("~")
+
+#fp,pathname,description=find_module('timeResolved',[home+'/Documents/python'])
+#TR=load_module('timeResolved',fp,pathname,description)
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='femtoPy'),
+      version='0.1',
+      description='Python code for data analysis in the ultrafast nanotools lab',
+    
