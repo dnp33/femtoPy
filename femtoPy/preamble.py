4468cfdaf76fbf853bab8310d55a51feb6b80620
David <purschke@ualberta.ca>
2017-01-18 16:05:02 -0700

    changed fontsizes

--- femtoPy/preamble.py
+++ femtoPy/preamble.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 
 plt.rcParams['figure.figsize']=(10,8)
 
-plt.rcParams['axes.labelsize']=20
-plt.rcParams['axes.titlesize']=25
+plt.rcParams['axes.labelsize']=23
+plt.rcParams['axes.titlesize']=30
 plt.rcParams['axes.grid']=True
-plt.rcParams['axes.linewidth']=4
+plt.rcParams['axes.linewidth']=3
 
 plt.rcParams['lines.linewidth']=3
 
-plt.rcParams['xtick.labelsize']=15
-plt.rcParams['ytick.labelsize']=15
+plt.rcParams['xtick.labelsize']=23
+plt.rcParams['ytick.labelsize']=23
 
 plt.rcParams['xtick.major.size']=10
 plt.rcParams['xtick.minor.size']=7
