ffad143b498895d5eb5c84157cfb328a84ebfff3
David <purschke@ualberta.ca>
2017-01-18 15:58:46 -0700

    added tick sizes to preamble

--- femtoPy/preamble.py
+++ femtoPy/preamble.py
@@ -13,6 +13,16 @@ plt.rcParams['lines.linewidth']=3
 plt.rcParams['xtick.labelsize']=15
 plt.rcParams['ytick.labelsize']=15
 
+plt.rcParams['xtick.major.size']=15
+plt.rcParams['xtick.minor.size']=12
+plt.rcParams['xtick.major.width']=2
+plt.rcParams['xtick.minor.width']=2
+
+plt.rcParams['ytick.major.size']=15
+plt.rcParams['ytick.minor.size']=12
+plt.rcParams['ytick.major.width']=2
+plt.rcParams['ytick.minor.width']=2
+
 plt.rcParams['legend.fontsize']=20
 plt.rcParams['legend.fancybox']=True
 
