19a1acd02e8879204250f99fbf44d216a501bf34
David <purschke@ualberta.ca>
2017-02-02 11:24:31 -0700

    added new figure functions

--- femtoPy/preamble.py
+++ femtoPy/preamble.py
@@ -1,5 +1,18 @@
 import numpy as np
+import matplotlib as matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.ticker
+
+def setTicks(ax):
+    locator=matplotlib.ticker.AutoMinorLocator(n=2)
+    ax.xaxis.set_minor_locator(locator)
+    ax.yaxis.set_minor_locator(locator)
+
+def newFig(x=10,y=8):
+    fig,ax=plt.subplots(figsize=(x,y))
+    setTicks(ax)
+
+    return fig,ax
 
 plt.rcParams['figure.figsize']=(10,8)
 
@@ -13,21 +26,19 @@ plt.rcParams['lines.linewidth']=3
 plt.rcParams['xtick.labelsize']=23
 plt.rcParams['ytick.labelsize']=23
 
-plt.rcParams['xtick.major.size']=10
-plt.rcParams['xtick.minor.size']=7
+plt.rcParams['xtick.major.size']=12
+plt.rcParams['xtick.minor.size']=6
 plt.rcParams['xtick.major.width']=2
 plt.rcParams['xtick.minor.width']=2
-plt.rcParams['xtick.minor.top']=True
-plt.rcParams['xtick.minor.bottom']=True
+plt.rcParams['xtick.minor.visible']=True
 
 
 
-plt.rcParams['ytick.major.size']=10
-plt.rcParams['ytick.minor.size']=7
+plt.rcParams['ytick.major.size']=12
+plt.rcParams['ytick.minor.size']=6
 plt.rcParams['ytick.major.width']=2
 plt.rcParams['ytick.minor.width']=2
-plt.rcParams['ytick.minor.top']=True
-plt.rcParams['ytick.minor.bottom']=True
+plt.rcParams['ytick.minor.visible']=True
 
 plt.rcParams['xtick.direction']='out'
 plt.rcParams['ytick.direction']='out'
